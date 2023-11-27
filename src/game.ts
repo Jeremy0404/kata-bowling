59906bb3fc386907cf515a79e57be07e5bbaeda9
Jérémy <jeremy.collombclerc@gmail.com>
2023-11-28 00:59:28 +0100

    Scores a game with all spares

--- src/game.ts
+++ src/game.ts
@@ -2,11 +2,28 @@ export class Game {
   constructor() {}
 
   printScore(resultString: string): number {
-    const turns = resultString.split(' ');
-    let score: number = 0;
-    turns.forEach((turn: string) => {
-      score += +turn.split('')[0];
-    });
+    const turnScores = resultString.split(' ');
+    let score = 0;
+
+    for (let i = 0; i < turnScores.length; i++) {
+      if (turnScores[i].includes('/')) {
+        score += 10;
+        if (i + 1 < turnScores.length) {
+          const nextTurnScore = turnScores[i + 1];
+          const nextThrow =
+            nextTurnScore[0] === '-' ? 0 : parseInt(nextTurnScore[0]);
+          score += nextThrow;
+        } else if (i === 9) {
+          const extraThrow =
+            turnScores[i].length > 2 ? parseInt(turnScores[i][2]) : 0;
+          score += extraThrow;
+        }
+      } else {
+        score += turnScores[i]
+          .split('')
+          .reduce((acc, val) => acc + (val === '-' ? 0 : parseInt(val)), 0);
+      }
+    }
 
     return score;
   }
--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,17 @@
 import { Handler } from '../src/handler';
 import { Game } from '../src/game';
 
-describe('bowling', () => {
-  it('scores without modifier', () => {
-    const handler = new Handler(new Game());
-    expect(handler.execute('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-')).toStrictEqual(90);
+function verify(scoresString: string, expected: number) {
+  const handler = new Handler(new Game());
+  expect(handler.execute(scoresString)).toStrictEqual(expected);
+}
+
+describe('Bowling Score Calculator', () => {
+  it('scores a game with no strikes or spares', () => {
+    verify('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-', 90);
+  });
+
+  it('scores a game with all spares', () => {
+    verify('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5', 150);
   });
 });
